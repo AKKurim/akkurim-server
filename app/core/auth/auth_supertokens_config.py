9ddabea13b9136c0d11fdac9140a7ec9abd84cb8
czmatejt <tajovsky.matej@gamil.com>
2025-04-13 23:42:48 +0200

    okay

--- app/core/auth/auth_supertokens_config.py
+++ app/core/auth/auth_supertokens_config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from supertokens_python import InputAppInfo, SupertokensConfig, init
 from supertokens_python.asyncio import get_user
 from supertokens_python.recipe import dashboard, emailpassword, session, userroles
@@ -7,27 +9,29 @@ from app.core.config import settings
 from app.core.logging import logger
 
 
-def custom_create_new_session(input_: RecipeInterface):
-    original_create = input_.create_new_session
+def override_create_new_session_function(original_implementation: RecipeInterface):
+    original_create = original_implementation.create_new_session
 
-    async def override_create_new_session(
-        user_id, access_token_payload=None, session_data=None, user_context=None
+    async def custom_create_new_session(
+        request,
+        user_id: str,
+        access_token_payload: Optional[dict] = None,
+        session_data: Optional[dict] = None,
+        user_context: Optional[dict] = None,
     ):
         access_token_payload = access_token_payload or {}
 
-        # Fetch user email using Supertokens user_id
-        logger.info("fetching user email")
         user = await get_user(user_id)
-        logger.info(f"fetched user email: {user}")
-        if user is not None:
+        logger.info(f"user  {user}")
+        if user:
             access_token_payload["email"] = user.email
 
         return await original_create(
-            user_id, access_token_payload, session_data, user_context
+            request, user_id, access_token_payload, session_data, user_context
         )
 
-    input_.create_new_session = override_create_new_session
-    return input_
+    original_implementation.create_new_session = custom_create_new_session
+    return original_implementation
 
 
 def supertokens_init():
