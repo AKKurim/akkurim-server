06ba9cb400317563b8d121b609474c372005c393
czmatejt <tajovsky.matej@gamil.com>
2025-04-13 23:46:48 +0200

    alright shit just wont work

--- app/core/auth/auth_supertokens_config.py
+++ app/core/auth/auth_supertokens_config.py
@@ -6,32 +6,6 @@ from supertokens_python.recipe import dashboard, emailpassword, session, userrol
 from supertokens_python.recipe.session.interfaces import RecipeInterface
 
 from app.core.config import settings
-from app.core.logging import logger
-
-
-def override_create_new_session_function(original_implementation: RecipeInterface):
-    original_create = original_implementation.create_new_session
-
-    async def custom_create_new_session(
-        request,
-        user_id: str,
-        access_token_payload: Optional[dict] = None,
-        session_data: Optional[dict] = None,
-        user_context: Optional[dict] = None,
-    ):
-        access_token_payload = access_token_payload or {}
-
-        user = await get_user(user_id)
-        logger.info(f"user  {user}")
-        if user:
-            access_token_payload["email"] = user.email
-
-        return await original_create(
-            request, user_id, access_token_payload, session_data, user_context
-        )
-
-    original_implementation.create_new_session = custom_create_new_session
-    return original_implementation
 
 
 def supertokens_init():
@@ -49,9 +23,6 @@ def supertokens_init():
         recipe_list=[
             session.init(
                 expose_access_token_to_frontend_in_cookie_based_auth=True,
-                override=session.InputOverrideConfig(
-                    functions=override_create_new_session_function
-                ),
             ),
             emailpassword.init(),
             dashboard.init(admins=[settings.DASHBOARD_ADMIN]),
