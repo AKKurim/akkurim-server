255f51c7782573ed653b8f7b80f1db569c32e52d
czmatejt <tajovsky.matej@gmail.com>
2025-07-27 19:14:58 +0200

    create new db in alembic

--- app/alembic/versions/f6ae4f69a234_recreate_db.py
+++ app/alembic/versions/f6ae4f69a234_recreate_db.py
@@ -1,25 +1,36 @@
 """recreate db
 
 Revision ID: f6ae4f69a234
-Revises: 
+Revises:
 Create Date: 2025-07-27 11:10:42.121248
 
 """
+
+import re
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'f6ae4f69a234'
+revision: str = "f6ae4f69a234"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    with open("/app/app/resources/sql/updated_schema.sql", "r", encoding="utf-8") as f:
+        sql = f.read()
+
+    with open("/app/app/resources/sql/active_tenants.txt", "r", encoding="utf-8") as f:
+        tenants = [each for each in f.read().split("\n") if each]
+
+    conn = op.get_bind()
+
+    for tenant_id in tenants:
+        sql = re.sub("tenant_id", tenant_id, sql)
+        conn.execute(sa.text(sql))
 
 
 def downgrade() -> None:
