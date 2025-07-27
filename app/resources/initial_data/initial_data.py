75e5c83b9642a7e87254539b20d4c34da9a53aee
czmatejt <tajovsky.matej@gmail.com>
2025-07-27 19:37:00 +0200

    huh

--- app/resources/initial_data/initial_data.py
+++ app/resources/initial_data/initial_data.py
@@ -10,7 +10,7 @@ async def main():
             db_ = connection
             await db_.execute(
                 "INSERT INTO public.remote_configs (id, urgent_message, show_from, show_to, minimum_app_version, deleted_at) "
-                + " VALUES 0, '', '2024-01-01T00:00:00+00:00', '2024-12-31T23:59:59+00:00', '2.4.0', null"
+                + " VALUES (0, '', '2024-01-01T00:00:00+00:00', '2024-12-31T23:59:59+00:00', '2.4.0', null)"
             )
     except Exception as e:
         print(e)
