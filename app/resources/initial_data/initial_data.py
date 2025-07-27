651ff2836f16db80af31716698fc7a9a497fd7e2
czmatejt <tajovsky.matej@gmail.com>
2025-07-27 19:03:52 +0200

    add init data

--- app/resources/initial_data/initial_data.py
+++ app/resources/initial_data/initial_data.py
@@ -3,15 +3,14 @@ from asyncio import get_event_loop
 from app.core.shared.database import db, get_db
 
 
-async def create_athlete_status(name: str, end_of_id: str):
+async def main():
     await db.connect()
     try:
         async with db.pool.acquire() as connection:
             db_ = connection
             await db_.execute(
-                "INSERT INTO akkurim.athlete_status (id, name) VALUES ($1, $2)",
-                "5f0e92e2-d123-11ef-9cd2-0242ac1200" + end_of_id,
-                name,
+                "INSERT INTO public.remote_configs (id, urgent_message, show_from, show_to, minimum_app_version, deleted_at) "
+                + " VALUES 0, '', '2024-01-01T00:00:00+00:00', '2024-12-31T23:59:59+00:00', '2.4.0', null"
             )
     except Exception as e:
         print(e)
@@ -20,11 +19,7 @@ async def create_athlete_status(name: str, end_of_id: str):
 
 
 if __name__ == "__main__":
-    # loop = get_event_loop()
-    # loop.run_until_complete(create_athlete_status("Active", "02"))
-    # loop.run_until_complete(create_athlete_status("Pending", "03"))
-    # loop.run_until_complete(create_athlete_status("Inactive", "04"))
-    # loop.run_until_complete(create_athlete_status("Archived", "05"))
-
-    # loop.close()
+    loop = get_event_loop()
+    main()
+    loop.close()
     pass
